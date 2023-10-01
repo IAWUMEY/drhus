a506592d1ded369d34af65595b39b10020ba0a90
IAWUMEY <isaacawumey92@gmail.com>
2023-10-01 16:49:53 -0700

    initial commit

---
+++ start.py
@@ -0,0 +1,15 @@
+# Starting with Variables and Datatypes in python
+first_number = 12
+second_number = 14
+total_sum = first_number + second_number
+product = first_number * second_number
+print('performing some computations in python...')
+print('='* 8)
+print()
+print(f'the value for the first number is:{first_number}')
+print(f'the value of the second number is:{second_number}')
+print()
+print(f'the sum of the first number and the second number is:{total_sum }')
+print()
+print(f'the product of first number and second number is:{product}')
+      
